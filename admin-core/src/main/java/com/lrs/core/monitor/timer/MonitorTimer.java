d98461655c3a095b176bdde1c6d53fbeebb4d849
rstyro <rstyro@gmail.com>
2019-06-20 21:15:52 +0800

    bug

--- admin-core/src/main/java/com/lrs/core/monitor/timer/MonitorTimer.java
+++ admin-core/src/main/java/com/lrs/core/monitor/timer/MonitorTimer.java
@@ -14,13 +14,22 @@ public class MonitorTimer {
 
     @Autowired
     private IServerService serverService;
+    private static final int modNumber =10;
 
-
+    @Scheduled(fixedDelay = 5*1000)
+    public void pingMod1(){
+        try {
+            log.info("===============1定时任务执行时间点："+ LocalDateTime.now());
+            serverService.monitor(modNumber,1);
+        }catch (Exception e){
+            log.error(e.getMessage(),e);
+        }
+    }
     @Scheduled(fixedDelay = 5*1000)
     public void pingMod2(){
         try {
-            System.out.println("===============2定时任务执行时间点："+ LocalDateTime.now());
-            serverService.monitor(2,0);
+            log.info("===============2定时任务执行时间点："+ LocalDateTime.now());
+            serverService.monitor(modNumber,2);
         }catch (Exception e){
             log.error(e.getMessage(),e);
         }
@@ -28,8 +37,71 @@ public class MonitorTimer {
     @Scheduled(fixedDelay = 5*1000)
     public void pingMod3(){
         try {
-            System.out.println("===============3定时任务执行时间点："+ LocalDateTime.now());
-            serverService.monitor(2,1);
+            log.info("===============3定时任务执行时间点："+ LocalDateTime.now());
+            serverService.monitor(modNumber,3);
+        }catch (Exception e){
+            log.error(e.getMessage(),e);
+        }
+    }
+    @Scheduled(fixedDelay = 5*1000)
+    public void pingMod4(){
+        try {
+            log.info("===============4定时任务执行时间点："+ LocalDateTime.now());
+            serverService.monitor(modNumber,4);
+        }catch (Exception e){
+            log.error(e.getMessage(),e);
+        }
+    }
+    @Scheduled(fixedDelay = 5*1000)
+    public void pingMod5(){
+        try {
+            log.info("===============5定时任务执行时间点："+ LocalDateTime.now());
+            serverService.monitor(modNumber,5);
+        }catch (Exception e){
+            log.error(e.getMessage(),e);
+        }
+    }
+    @Scheduled(fixedDelay = 5*1000)
+    public void pingMod6(){
+        try {
+            log.info("===============6定时任务执行时间点："+ LocalDateTime.now());
+            serverService.monitor(modNumber,6);
+        }catch (Exception e){
+            log.error(e.getMessage(),e);
+        }
+    }
+    @Scheduled(fixedDelay = 5*1000)
+    public void pingMod7(){
+        try {
+            log.info("===============7定时任务执行时间点："+ LocalDateTime.now());
+            serverService.monitor(modNumber,7);
+        }catch (Exception e){
+            log.error(e.getMessage(),e);
+        }
+    }
+    @Scheduled(fixedDelay = 5*1000)
+    public void pingMod8(){
+        try {
+            log.info("===============8定时任务执行时间点："+ LocalDateTime.now());
+            serverService.monitor(modNumber,8);
+        }catch (Exception e){
+            log.error(e.getMessage(),e);
+        }
+    }
+    @Scheduled(fixedDelay = 5*1000)
+    public void pingMod9(){
+        try {
+            log.info("===============9定时任务执行时间点："+ LocalDateTime.now());
+            serverService.monitor(modNumber,9);
+        }catch (Exception e){
+            log.error(e.getMessage(),e);
+        }
+    }
+    @Scheduled(fixedDelay = 5*1000)
+    public void pingMod10(){
+        try {
+            log.info("===============0定时任务执行时间点："+ LocalDateTime.now());
+            serverService.monitor(modNumber,0);
         }catch (Exception e){
             log.error(e.getMessage(),e);
         }
